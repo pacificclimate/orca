19566b374f975a83938da74fda2401d8e8a45e91
nikola-rados <nrados@uvic.ca>
2021-02-11 15:42:16 -0800

    Create scripts dir and module

---
+++ scripts/process.py
@@ -0,0 +1,23 @@
+import click
+from orca import finder
+
+
+@click.command()
+@click.option(
+    "-x",
+    "--connection-string",
+    help="Database connection string",
+    default="postgres://ce_meta_ro@db3.pcic.uvic.ca/ce_meta_12f290b63791",
+)
+@click.option(
+    "-f",
+    "--filename",
+    help="Filename to search for in DB"
+)
+def process(connection_string, filename):
+    datafile = finder.find_filepath(connection_string, filename)
+    print(datafile)
+
+
+if __name__ == "__main__":
+    process()
