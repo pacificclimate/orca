fe000d841b65ab8083711e67653c0afecadc09c4
nikola-rados <nrados@uvic.ca>
2021-02-11 15:42:07 -0800

    Rename module

---
+++ orca/finder.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from modelmeta import DataFile
+
+
+def find_filepath(connection_string, filename):
+    """Given a filename, search the ce_meta database for a match"""
+    Session = sessionmaker(create_engine(connection_string))
+    sesh = Session()
+
+    try:
+        datafile = sesh.query(DataFile).filter(DataFile.unique_id == filename)
+    except Exception as e:
+        raise e
+
+    return datafile.first()
