9b763b4eec4d8e5d67607f8320141c01428c9589
nikola-rados <nrados@uvic.ca>
2021-02-12 14:26:13 -0800

    New url format

--- orca/request_opendap.py
+++ orca/request_opendap.py
@@ -1,27 +1,30 @@
 import requests
-from orca import finder
+import re
+
 
 def request_opendap(url):
-    r = requests.get(data_url)
+    r = requests.get(url)
+    print(r.content)
     if r.status_code==200:
         return "sucess"
     else:
         return "failed"
 
 
-def build_url(
-    storage_path,
-    var,
-    lat_end,
-    lon_end,
-    time_end=55151,
-    time_start=0,
-    lat_start=0,
-    lon_start=0,
-):
+def build_url(filepath, variable, lat, lon):
     thredds_base = (
         "https://docker-dev03.pcic.uvic.ca/twitcher/ows/proxy/thredds/dodsC/datasets"
     )
-    data_url = f"{thredds_base}{storage_path}?{var}[{time_start}:{time_end}][{lon_start}:{lon_end}][{lat_start}:{lat_end}]&"
+    variable_form = re.compile('([a-z]*)(\[\d*:\d*\])')
+    var_name, var_time = variable_form.match(variable).group(0)
+    print(var_name, var_time)
+    import sys
+    sys.exit(0)
+
+
+    # Expected format
+    # ?lon[0:1:1067],lat[0:1:509],time[0:1:55114],tasmax[0:1:0][0:1:0][0:1:0]
+    # hard-coding some stuff for now
+    data_url = f"{thredds_base}{storage_path}?lon{lon},lat{lat},time{var_time},{var_name}[0:1:0][0:1:0][0:1:0]"
 
     return data_url
