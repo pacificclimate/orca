7f764127a07016da858a063d51a41f7d8218434e
nikola-rados <nrados@uvic.ca>
2021-02-12 14:25:55 -0800

    Rewrite process script

--- scripts/process.py
+++ scripts/process.py
@@ -1,5 +1,5 @@
 import click
-from orca import finder, request_opendap
+from orca.process import process_request
 
 
 @click.command()
@@ -10,39 +10,12 @@ from orca import finder, request_opendap
     default="postgresql://httpd_meta@db3.pcic.uvic.ca/pcic_meta",
 )
 @click.option("-u", "--unique-id", help="Unique_id to search for in DB")
-@click.option("-v", "--variable", help="Variable of interest")
-@click.option("-s", "--time_start", default=0, help="Start time")
-@click.option("-e", "--time_end", default=55151, help="End time")
-@click.option("-las", "--lat_start", default=0, help="Start longitude")
-@click.option("-los", "--lon_start", default=0, help="Start longitude")
-@click.option("-lae", "--lat_end", help="End latitude")
-@click.option("-loe", "--lon_end", help="End longitude")
-def process(
-    connection_string,
-    unique_id,
-    variable,
-    time_start,
-    time_end,
-    lat_end,
-    lon_end,
-    lat_start,
-    lon_start,
-):
-    filepath = finder.find_filepath(connection_string, unique_id)
-    print(filepath)
-    url = request_opendap.build_url(
-        filepath,
-        variable,
-        lat_end,
-        lon_end,
-        time_end,
-        time_start,
-        lat_start,
-        lon_start,
-    )
-    print(url)
-    r_status = request_opendap.request_opendap(url)
-    print(r_status)
+@click.option("-v", "--variable", help="Varible + time range string (ex. tasmax[0:55114])")
+@click.option("-t", "--lat", help="Latitude slab as index (ex. [90:100])")
+@click.option("-n", "--lon", help="Longitude slab as index (ex. [90:100])")
+def process(connection_string, unique_id, variable, lat, lon):
+    """CLI for orca"""
+    process_request(connection_string, unique_id, variable, lat, lon)
 
 
 if __name__ == "__main__":
