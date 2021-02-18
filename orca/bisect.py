5044230f84ef11e154322949bc3092af3ad4d65b
cairosanders <cairosanders@hotmail.com>
2021-02-18 14:56:57 -0800

    Adjust bisect to extract variable time slicing

--- orca/bisect.py
+++ orca/bisect.py
@@ -2,13 +2,14 @@ import re
 
 
 def bisect_request(url):
-    start_end_format = re.compile(r"(time\[(\d*)(:\d*){0,1}:(\d*)\])")
-    time, start, step, end = start_end_format.findall(url)[0]
+    start_end_format = re.compile(r"(([a-z]+)\[(\d*)(:\d*){0,1}:(\d*)\])")
+    print("\n" + str(start_end_format.findall(url)[0]) + "\n")
+    var_time, var, start, step, end = start_end_format.findall(url)[0]
 
     mid = (int(start) + int(end)) // 2
 
     # step will either be ':n' or '' if there is no step
-    url1 = url.replace(time, f"time[{start}{step}:{mid}]")
-    url2 = url.replace(time, f"time[{mid+1}{step}:{end}]")
+    url1 = url.replace(var_time, f"{var}[{start}{step}:{mid}]")
+    url2 = url.replace(var_time, f"{var}[{mid+1}{step}:{end}]")
 
-    return url1, url2
+    return [url1, url2]
--- tests/test_bisect.py
+++ tests/test_bisect.py
@@ -3,12 +3,18 @@ from orca.bisect import bisect_request
 
 
 @pytest.mark.parametrize(
-    ("url"),
+    ("root"),
     [
-        "https://docker-dev03.pcic.uvic.ca/twitcher/ows/proxy/thredds/dodsC/datasets/storage/data/climate/downscale/BCCAQ2/bccaqv2_with_metadata/tasmax_day_BCCAQv2+ANUSPLIN300_bcc-csm1-1-m_historical+rcp26_r1i1p1_19500101-21001231.nc?lon[206:206],lat[91:91],time[0:55113],tasmax[0:1:0][0:1:0][0:1:0]",
-        "https://docker-dev03.pcic.uvic.ca/twitcher/ows/proxy/thredds/dodsC/datasets/storage/data/climate/downscale/BCCAQ2/bccaqv2_with_metadata/tasmax_day_BCCAQv2+ANUSPLIN300_bcc-csm1-1-m_historical+rcp26_r1i1p1_19500101-21001231.nc?lon[206:206],lat[91:91],time[0:1:55114],tasmax[0:1:0][0:1:0][0:1:0]",
+        "https://docker-dev03.pcic.uvic.ca/twitcher/ows/proxy/thredds/dodsC/datasets/storage/data/climate/downscale/BCCAQ2/bccaqv2_with_metadata/tasmax_day_BCCAQv2+ANUSPLIN300_bcc-csm1-1-m_historical+rcp26_r1i1p1_19500101-21001231.nc?<PARAMS>[0:1:509][0:1:1067]",
     ],
 )
-def test_bisect_request(url):
-    urls = bisect_request(url)
-    print(urls)
+@pytest.mark.parametrize(
+    ("var", "start", "step", "end"),
+    [("tasmin", 0, "", 55113), ("tasmax", 0, ":1", 55114), ("pr", 78, ":5", 100)],
+)
+def test_bisect_request(root, var, start, step, end):
+    url = root.replace("<PARAMS>", f"{var}[{start}{step}:{end}]")
+    url1, url2 = bisect_request(url)
+    mid = (start + end) // 2
+    assert f"{var}[{start}{step}:{mid}]" in url1
+    assert f"{var}[{mid+1}{step}:{end}]" in url2
