2221309e50eb5e0cc1f97f1a9f8d5ae4d1f09ce3
cairosanders <cairosanders@hotmail.com>
2021-02-22 09:53:25 -0800

    Create split_url and test

--- orca/bisect.py
+++ orca/bisect.py
@@ -3,7 +3,6 @@ import re
 
 def bisect_request(url):
     start_end_format = re.compile(r"(([a-z]+)\[(\d*)(:\d*){0,1}:(\d*)\])")
-    print("\n" + str(start_end_format.findall(url)[0]) + "\n")
     var_time, var, start, step, end = start_end_format.findall(url)[0]
 
     mid = (int(start) + int(end)) // 2
@@ -13,3 +12,30 @@ def bisect_request(url):
     url2 = url.replace(var_time, f"{var}[{mid+1}{step}:{end}]")
 
     return [url1, url2]
+
+
+def split_url(url, size):
+    start_end_format = re.compile(r"(([a-z]+)\[(\d*)(:\d*){0,1}:(\d*)\])")
+    var_time, var, start, stride, end = start_end_format.findall(url)[0]
+    start = int(start)
+    end = int(end)
+
+    # Server error reports dataset size = dataset.nbytes/2
+    b = size / 2
+    # Request limit is 5e+8 bytes
+    chunks = -(-b // 5e8)  # take cieling
+    total = end - start
+    step = int(total // chunks)
+
+    chunk_end = start + step
+    urls = []
+
+    while chunk_end < end:
+        urls.append(url.replace(var_time, f"{var}[{start}{stride}:{chunk_end}]"))
+        start = chunk_end + 1
+        chunk_end += step
+
+    if start <= end:
+        urls.append(url.replace(var_time, f"{var}[{start}{stride}:{end}]"))
+
+    return urls
--- tests/test_bisect.py
+++ tests/test_bisect.py
@@ -1,5 +1,5 @@
 import pytest
-from orca.bisect import bisect_request
+from orca.bisect import bisect_request, split_url
 
 
 @pytest.mark.parametrize(
@@ -18,3 +18,26 @@ def test_bisect_request(root, var, start, step, end):
     mid = (start + end) // 2
     assert f"{var}[{start}{step}:{mid}]" in url1
     assert f"{var}[{mid+1}{step}:{end}]" in url2
+
+
+@pytest.mark.parametrize(
+    ("url", "size"),
+    [
+        (
+            "https://docker-dev03.pcic.uvic.ca/twitcher/ows/proxy/thredds/dodsC/datasets/storage/data/climate/downscale/BCCAQ2/bccaqv2_with_metadata/tasmax_day_BCCAQv2+ANUSPLIN300_HadGEM2-CC_historical+rcp45_r1i1p1_19500101-21001231.nc?tasmax[0:1:450][0:1:509][0:1:1067]",
+            982602720,
+        ),
+        (
+            "https://docker-dev03.pcic.uvic.ca/twitcher/ows/proxy/thredds/dodsC/datasets/storage/data/climate/downscale/BCCAQ2/bccaqv2_with_metadata/tasmin_day_BCCAQv2+ANUSPLIN300_inmcm4_historical+rcp85_r1i1p1_19500101-21001231.nc?tasmin[100:1:1500][0:1:509][0:1:1067]",
+            3052386720,
+        ),
+        (
+            "https://docker-dev03.pcic.uvic.ca/twitcher/ows/proxy/thredds/dodsC/datasets/storage/data/climate/downscale/BCCAQ2/bccaqv2_with_metadata/tasmin_day_BCCAQv2+ANUSPLIN300_inmcm4_historical+rcp85_r1i1p1_19500101-21001231.nc?tasmin[500:1:1100][0:1:509][0:1:1067]",
+            1309410720,
+        ),
+    ],
+)
+def test_split_url(url, size):
+    urls = split_url(url, size)
+    expected_chunks = -(-(size / 2) // 5e8)
+    assert len(urls) == expected_chunks
