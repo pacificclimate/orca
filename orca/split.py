b2f2165173afa8649edad7eb23d826a934ec41fc
cairosanders <cairosanders@hotmail.com>
2021-02-22 11:17:08 -0800

    Remove bisect and rename bisect.py > split.py

--- orca/split.py
+++ orca/split.py
@@ -1,19 +1,6 @@
 import re
 
 
-def bisect_request(url):
-    start_end_format = re.compile(r"(([a-z]+)\[(\d*)(:\d*){0,1}:(\d*)\])")
-    var_time, var, start, step, end = start_end_format.findall(url)[0]
-
-    mid = (int(start) + int(end)) // 2
-
-    # step will either be ':n' or '' if there is no step
-    url1 = url.replace(var_time, f"{var}[{start}{step}:{mid}]")
-    url2 = url.replace(var_time, f"{var}[{mid+1}{step}:{end}]")
-
-    return [url1, url2]
-
-
 def split_url(url, size):
     start_end_format = re.compile(r"(([a-z]+)\[(\d*)(:\d*){0,1}:(\d*)\])")
     var_time, var, start, stride, end = start_end_format.findall(url)[0]
