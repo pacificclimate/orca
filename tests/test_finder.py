19357b8192647d0d48d78e3b4f984c4b20001106
nikola-rados <nrados@uvic.ca>
2021-02-23 13:20:36 -0800

    Ensure 100% coverage

--- tests/test_finder.py
+++ tests/test_finder.py
@@ -1,11 +1,37 @@
 import pytest
 from orca.finder import find_filepath
 
-@pytest.mark.parametrize("unique_id, expected", [
-    ("unique_id_0", "data_file_0"),
-    ("unique_id_1", "data_file_1"),
-    ("unique_id_2", "data_file_2"),
-])
+
+@pytest.mark.parametrize(
+    "unique_id, expected",
+    [
+        ("unique_id_0", "data_file_0"),
+        ("unique_id_1", "data_file_1"),
+        ("unique_id_2", "data_file_2"),
+    ],
+)
 def test_find_filepath(test_session, unique_id, expected):
     filename = find_filepath(test_session, unique_id)
     assert filename == expected
+
+
+@pytest.mark.parametrize(
+    "bad_unique_id",
+    [
+        (0),  # search expects a string so an int will break it
+    ],
+)
+def test_find_filepath_bad_search(test_session, bad_unique_id):
+    with pytest.raises(Exception):
+        filename = find_filepath(test_session, bad_unique_id)
+
+
+@pytest.mark.parametrize(
+    "no_match_id",
+    [
+        ("no_match_id"),
+    ],
+)
+def test_find_filepath_no_match(test_session, no_match_id):
+    with pytest.raises(Exception):
+        filename = find_filepath(test_session, no_match_id)
