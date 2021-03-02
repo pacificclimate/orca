222089baf2b43970745af2cc138646800a6bbed9
nikola-rados <nrados@uvic.ca>
2021-03-02 14:41:42 -0800

    Rework args

--- orca/main.py
+++ orca/main.py
@@ -1,26 +1,26 @@
 from .db_handler import find_filepath, start_session
-from .requester import build_url, file_from_opendap
+from .requester import build_url, file_from_opendap, decompose_url
 from .utils import setup_logging
 
 
 def orc(
-    connection_string,
+    url,
     unique_id,
-    variable,
-    lat,
-    lon,
+    connection_string="postgresql://httpd_meta@db3.pcic.uvic.ca/pcic_meta",
     thredds_base="https://docker-dev03.pcic.uvic.ca/twitcher/ows/proxy/thredds/dodsC/datasets",
     outfile="outfile.nc",
     log_level="INFO",
 ):
+    # def orc(unique_id, url):
     """OPeNDAP Request Complier
 
     Builds an OPeNDAP request that can be split into a series of requests and
     compiled back into a single output.
     """
     logger = setup_logging(log_level)
-
     logger.info("Processing data file request")
+
+    variable, lat, lon = decompose_url(url)
 
     logger.debug("Starting db session")
     sesh = start_session(connection_string)
--- scripts/process.py
+++ scripts/process.py
@@ -3,18 +3,14 @@ from orca.main import orc
 
 
 @click.command()
+@click.option("--url", help="Data portal URL")
+@click.option("--unique-id", help="Unique_id to search for in DB")
 @click.option(
     "-x",
     "--connection-string",
     help="Database connection string",
     default="postgresql://httpd_meta@db3.pcic.uvic.ca/pcic_meta",
 )
-@click.option("-u", "--unique-id", help="Unique_id to search for in DB")
-@click.option(
-    "-v", "--variable", help="Varible + time range string (ex. tasmax[0:55114])"
-)
-@click.option("-t", "--lat", help="Latitude slab as index (ex. [89:89])")
-@click.option("-n", "--lon", help="Longitude slab as index (ex. [211:211])")
 @click.option(
     "-b",
     "--thredds-base",
@@ -23,20 +19,9 @@ from orca.main import orc
 )
 @click.option("-o", "--outfile", help="Output file path", default="outfile.nc")
 @click.option("-l", "--log-level", help="Ouput file path", default="INFO")
-def process(
-    connection_string, unique_id, variable, lat, lon, thredds_base, outfile, log_level
-):
+def process(url, unique_id, connection_string, thredds_base, outfile, log_level):
     """CLI for orca"""
-    orc(
-        connection_string,
-        unique_id,
-        variable,
-        lat,
-        lon,
-        thredds_base,
-        outfile,
-        log_level,
-    )
+    orc(url, unique_id, connection_string, thredds_base, outfile, log_level)
 
 
 if __name__ == "__main__":
