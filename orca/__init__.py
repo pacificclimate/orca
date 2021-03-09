f81f560434b880668a61f667d02187c781bb93af
nikola-rados <nrados@uvic.ca>
2021-03-09 11:08:29 -0800

    Create app factory

---
+++ orca/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask, send_file
+from orca.main import orc
+from orca.utils import get_filename_from_path
+
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route("/orca/<string:unique_id>/<string:targets>", methods=["GET", "POST"])
+    def orca(unique_id, targets):
+        outpath = orc(unique_id, targets)
+
+        return send_file(
+            outpath,
+            mimetype="application/x-netcdf",
+            as_attachment=True,
+            attachment_filename=get_filename_from_path(outpath),
+        )
+
+    return app
