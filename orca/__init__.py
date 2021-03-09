9ec6035bcf9474ff7afeb6c5dca02da80c90e288
nikola-rados <nrados@uvic.ca>
2021-03-09 12:11:51 -0800

    Add simple config

--- orca/__init__.py
+++ orca/__init__.py
@@ -1,20 +1,12 @@
-from flask import Flask, send_file
-from orca.main import orc
-from orca.utils import get_filename_from_path
+from flask import Flask
 
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_pyfile("config.py")
 
-    @app.route("/orca/<string:unique_id>/<string:targets>", methods=["GET", "POST"])
-    def orca(unique_id, targets):
-        outpath = orc(unique_id, targets)
+    from orca.routes import data
 
-        return send_file(
-            outpath,
-            mimetype="application/x-netcdf",
-            as_attachment=True,
-            attachment_filename=get_filename_from_path(outpath),
-        )
+    app.register_blueprint(data)
 
     return app
