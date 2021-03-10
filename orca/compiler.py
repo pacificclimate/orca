342946797c558cb50859368dc8613a7ee3ee522f
nikola-rados <nrados@uvic.ca>
2021-03-10 10:09:55 -0800

    Separate functional code from routing

---
+++ orca/compiler.py
@@ -0,0 +1,49 @@
+"""The compiler module runs through all the steps to produce an ORCA output
+
+Set up database
+|
+Search for filepath in database
+|
+Construct OPeNDAP URL with filepath
+|
+"Smart" download data (this is using the splitting and recompiling strategy)
+|
+Return
+"""
+
+from orca.db_handler import find_filepath, start_session
+from orca.requester import build_opendap_url, file_from_opendap
+from orca.utils import setup_logging
+
+
+def orc(
+    unique_id,
+    targets,
+    thredds_base="https://docker-dev03.pcic.uvic.ca/twitcher/ows/proxy/thredds/dodsC/datasets",
+    outdir="/tmp/",
+    outfile="",
+    log_level="INFO",
+):
+    """OPeNDAP Request Complier
+
+    Builds an OPeNDAP request that can be split into a series of requests and
+    compiled back into a single output. This output is written to the `outpath`
+    location and that same location is returned from this method.
+    """
+    logger = setup_logging(log_level)
+    logger.info("Processing data file request")
+
+    logger.debug("Starting db session")
+    sesh = start_session()
+    filepath = find_filepath(sesh, unique_id)
+    logger.debug(f"Got filepath: {filepath}")
+
+    opendap_url = build_opendap_url(thredds_base, filepath, targets)
+    logger.debug(f"Initial OPeNDAP URL: {opendap_url}")
+
+    logger.info("Downloading data from OPeNDAP")
+    outpath = file_from_opendap(opendap_url, outdir, outfile)
+    logger.debug(f"Result avaialble at {outpath}")
+
+    logger.info("Complete")
+    return outpath
--- orca/routes.py
+++ orca/routes.py
@@ -1,45 +1,9 @@
 """Defines all routes available to Flask app"""
 
-from flask import Blueprint, send_file
-from orca.db_handler import find_filepath, start_session
-from orca.requester import build_opendap_url, file_from_opendap
-from orca.utils import setup_logging, get_filename_from_path
+from orca.utils import get_filename_from_path
 
 
 data = Blueprint("data", __name__, url_prefix="/data")
-
-
-def orc(
-    unique_id,
-    targets,
-    thredds_base="https://docker-dev03.pcic.uvic.ca/twitcher/ows/proxy/thredds/dodsC/datasets",
-    outdir="/tmp/",
-    outfile="",
-    log_level="INFO",
-):
-    """OPeNDAP Request Complier
-
-    Builds an OPeNDAP request that can be split into a series of requests and
-    compiled back into a single output. This output is written to the `outpath`
-    location and that same location is returned from this method.
-    """
-    logger = setup_logging(log_level)
-    logger.info("Processing data file request")
-
-    logger.debug("Starting db session")
-    sesh = start_session()
-    filepath = find_filepath(sesh, unique_id)
-    logger.debug(f"Got filepath: {filepath}")
-
-    opendap_url = build_opendap_url(thredds_base, filepath, targets)
-    logger.debug(f"Initial OPeNDAP URL: {opendap_url}")
-
-    logger.info("Downloading data from OPeNDAP")
-    outpath = file_from_opendap(opendap_url, outdir, outfile)
-    logger.debug(f"Result avaialble at {outpath}")
-
-    logger.info("Complete")
-    return outpath
 
 
 @data.route("/<string:unique_id>/<string:targets>", methods=["GET", "POST"])
