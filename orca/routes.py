3b8beb7691bc2f1d6539eeb004a73f3567e38853
nikola-rados <nrados@uvic.ca>
2021-03-18 12:48:11 -0700

    Rework data route

--- orca/routes.py
+++ orca/routes.py
@@ -1,6 +1,7 @@
 """Defines all routes available to Flask app"""
 
 from flask import Blueprint, send_file
+from urllib.parse import unquote
 from orca.compiler import orc
 from orca.utils import get_filename_from_path
 
@@ -8,10 +9,13 @@ from orca.utils import get_filename_from_path
 data = Blueprint("data", __name__, url_prefix="/data")
 
 
-@data.route("/<string:unique_id>/<string:targets>", methods=["GET", "POST"])
-def orc_route(unique_id, targets):
+@data.route("/<path:filepath>:<string:targets>", methods=["GET", "POST"])
+def orc_route(filepath, targets):
     """Wraps orc into a usable route with simplified inputs"""
-    outpath = orc(unique_id, targets, log_level="DEBUG")
+    filepath = unquote(filepath)
+    targets = unquote(targets)
+
+    outpath = orc(filepath, targets, log_level="DEBUG")
 
     return send_file(
         outpath,
