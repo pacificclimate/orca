766a170baae3af684558c7f625d2893673bb9cd4
Lee Zeman <lzeman@uvic.ca>
2025-09-24 09:49:40 -0700

    try getting thredds base from environment variable

--- orca/routes.py
+++ orca/routes.py
@@ -17,9 +17,9 @@ def orc_route():
     """Wraps orc into a usable route with simplified inputs"""
     filepath = request.args.get("filepath")
     targets = request.args.get("targets", None)
-    thredds_base = request.args.get(
-        "thredds_base",
-        "https://marble-dev01.pcic.uvic.ca/twitcher/ows/proxy/thredds/dodsC/datasets",
+    thredds_base = os.getenv(
+        "THREDDS_BASE",
+        default="https://marble-dev01.pcic.uvic.ca/twitcher/ows/proxy/thredds/dodsC/datasets",
     )
     threshold = request.args.get("threshold", 5e8)
     outdir = request.args.get("outdir", os.getenv("TMPDIR", default="/tmp/"))
